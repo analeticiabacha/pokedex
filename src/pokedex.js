dde25d361f11dc4535f1bdfb1787a3a8549500bc
Ana Leticia Bacha Prince <30864314+analeticiabacha@users.noreply.github.com>
2023-09-11 17:56:09 -0300

    fetch e definição dos pokemons

--- src/pokedex.js
+++ src/pokedex.js
@@ -1,10 +1,34 @@
 const pokeContainer = document.querySelector("#pokeContainer");
+const pokemonCount = 150;
+const colors = {
+    fire: '#FDDFDF',
+    grass: '#DEFDE0',
+    electric: '#FCF7DE',
+    water: '#DEF3FD',
+    ground: '#f4e7da',
+    rock: '#d5d5d4',
+    fairy: '#fceaff',
+    poison: '#98d7a5',
+    bug: '#f8d5a3',
+    dragon: '#97b3e6',
+    psychic: '#eaeda1',
+    flying: '#F5F5F5',
+    fighting: '#E6E0D4',
+    normal: '#F5F5F5'
+}
+const mainTypes = Object.keys(colors);
 
-const getPokemons = async () => {
-    const url = `https://pokeapi.co/api/v2/pokemon`;
+const fetchPokemons = async () => {
+    for (let i = 1; i <= pokemonCount; i++) {
+        await getPokemons(i); //26
+    }
+}
+
+const getPokemons = async (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon${id}`;
     const resp = await fetch(url);
     const data = await resp.json();
     console.log(data);
 }
-
-getPokemons();
+fetchPokemons();
+// getPokemons();
