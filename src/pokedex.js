9212b1e272a4d2736ebcc63bfa4807cb466f38df
Ana Leticia Bacha Prince <30864314+analeticiabacha@users.noreply.github.com>
2023-09-28 15:50:10 -0300

    settings

--- src/pokedex.js
+++ src/pokedex.js
@@ -28,7 +28,37 @@ const getPokemons = async (id) => {
     const url = `https://pokeapi.co/api/v2/pokemon${id}`;
     const resp = await fetch(url);
     const data = await resp.json();
-    console.log(data);
+    // console.log(data.types[0].type.name);
+    createPokemonCard(data);
 }
+
+const createPokemonCard = (poke) => {
+    const card = document.createElement('div');
+    card.classList.add('pokemon');
+
+    const name = poke.name[0].toUpperCase() + poke.name.slice(1);
+    const id = poke.id.toString().padStart(3, '0');
+
+    const pokeTypes = poke.types.map(type => type.type.name);
+    const type = mainTypes.find(type => pokeTypes.indexOf(type) > -1);
+    const color = colors[type];
+
+    card.style.backgroundColor = color;
+
+    const pokemonInnerHTML = `
+        <div class="imgContainer">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${poke.id}.png" alt="${name}">
+        </div>
+        <div class="info">
+            <span class="number">#${id}</span>
+            <h3 class="name">${name}</h3>
+            <small class="type">Type: <span>${type}</span></small>
+        </div>
+    `
+
+    card.innerHTML = pokemonInnerHTML;
+    pokeContainer.appendChild(card);
+}
+
 fetchPokemons();
 // getPokemons();
